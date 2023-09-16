447cd3147fc4c219f7e3bcb3f7270029f24905b1
Harsh Dixit <harshdixit573@gmail.com>
2023-09-16 06:46:50 +0530

    firebase deployment added

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -11,16 +11,18 @@ const Login = () => {
   const email=useRef(null)
   const password=useRef(null)
   const handleListner=()=>{
+    console.log(password.current.value);
     setErrorMessege(checkValidData( email.current.value,password.current.value))//why no destructuring
   }
   return (
     <div className='absolute'>
-        <div>
-            <form onSubmit={(e)=>e.preventDefault()} className=' rounded-md shadow-md bg-opacity-90 bg-black text-white absolute mx-[500px]  my-28 px-16 py-10' action="">
+        <div className='flex justify-center'>
+        <img className='bg-gradient-to-r  from-cyan-500 to-blue-500' src={NETFLIX_BACKGROUND_URL} alt="" srcset="" />
+            <form onSubmit={(e)=>e.preventDefault()} className='rounded-md shadow-md bg-opacity-90 max-w-md mt-28 bg-black text-white absolute px-16 py-10' action="">
             {isSignin?<h1 className='font-semibold text-4xl my-10'>Sign In</h1>:<h1 className='font-semibold text-4xl my-10'>Sign Up</h1>}
-            {!isSignin&&<input className=' rounded-md w-full bg-gray-700 mt-8  p-4 ' type='text' placeholder='Name'/>}
-                <input ref={email}  className='  rounded-md w-full bg-gray-700 mt-10 p-4' placeholder='Email id' type="text" />
-                <input ref={password} className=' rounded-md w-full bg-gray-700 mt-8  p-4 ' type='password' placeholder='Password'/>
+            {!isSignin&&<input className='focus:outline-none rounded-md shadow-lg w-full bg-gray-700 mt-8  p-4 ' type='text' placeholder='Name'/>}
+                <input ref={email}  className=' focus:outline-none shadow-lg rounded-md w-full bg-gray-700 mt-10 p-4' placeholder='Email id' type="text" />
+                <input ref={password} className='focus:outline-none shadow-lg rounded-md w-full bg-gray-700 mt-8  p-4 ' type='password' placeholder='Password'/>
                 {errorMessege&&<p className=' px-2 pt-8 text-red-600'>* {errorMessege}</p>}
                 <button onClick={handleListner} className=' font-bold w-full rounded-md mt-8 p-4 bg-red-600'>{isSignin?'Sign-In':'Sign-Up'}</button>
                 <div className='flex gap-2'>
@@ -29,7 +31,6 @@ const Login = () => {
                 </div>
             </form> 
         </div>
-        <img className='bg-gradient-to-r from-cyan-500 to-blue-500' src={NETFLIX_BACKGROUND_URL} alt="" srcset="" />
         </div>
           )
 }
---
+++ src/Utills/Firebase.jsx
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA5J2cY95v_XvgLSCQOWOpARzti-O86rlI",
+  authDomain: "netflix-gpt-2a6a0.firebaseapp.com",
+  projectId: "netflix-gpt-2a6a0",
+  storageBucket: "netflix-gpt-2a6a0.appspot.com",
+  messagingSenderId: "687587228882",
+  appId: "1:687587228882:web:01025074be2671d921810b",
+  measurementId: "G-W3Y1B09KJR"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
